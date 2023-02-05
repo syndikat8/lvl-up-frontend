2f6b2d2bce877e60696df8300f064bb603f1206f
dzmitryMatusik <dzmitry.matusik@cbitech.ae>
2023-02-05 20:27:52 +0300

    feat: add lesson 8 (classNames)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 import './styles/index.scss'
 
@@ -12,7 +13,7 @@ const App = () => {
     const { theme, changeTheme } = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <div onClick={changeTheme}>TOGGLE</div>
 
             <Link to={'/'}>Главная</Link>
