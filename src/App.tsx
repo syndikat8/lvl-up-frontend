78ba9b1496d0c10abf956984a40c8ac3950817f4
dzmitryMatusik <dzmitry.matusik@cbitech.ae>
2023-02-05 17:04:47 +0300

    feat: add lesson 5 (css module)

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Counter from "./components/Counter";
+import './index.scss'
+
+const App = () => {
+    return (
+        <div>
+            <div className="app">
+                TEST
+            </div>
+
+            <Counter/>
+        </div>
+    );
+};
+
+export default App;
